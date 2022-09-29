8a27530725205829e0f4f10e255570216adb72c4
ModarYaghiPet <111871976+ModarYaghiPet@users.noreply.github.com>
2022-09-30 01:02:29 +0200

    final working backend without front-end

--- migrations/Version20220929225209.php
+++ migrations/Version20220929225209.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220606131622 extends AbstractMigration
+final class Version20220929225209 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220606131622 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD email2 VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6494D5A9954 ON user (email2)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('DROP INDEX UNIQ_8D93D6494D5A9954 ON user');
+        $this->addSql('ALTER TABLE user DROP email2');
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
+//echo 1;
+//phpinfo();
+
 
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+//dd($_SERVER);
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
